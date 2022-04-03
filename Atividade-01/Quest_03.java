48cc0a5936e53c2e60aa30f8a7a509316dfd5e41
Kelvyn <jkelvynsantos@gmail.com>
2022-04-03 19:47:04 -0300

    adicionando questoes da primeira atividade

---
+++ Atividade-01/Quest_03.java
@@ -0,0 +1,30 @@
+package LP2;
+
+import java.util.Scanner;
+  
+public class Quest_03 {
+
+   public static void main(String[] args) {
+    
+       Scanner sc = new Scanner(System.in);
+       int op;
+
+       System.out.println("Digite um número:");
+       op = sc.nextInt();
+
+       switch (op) {
+
+           case 1:
+               System.out.println(1);
+               break;
+           case 2:
+               System.out.println(2);
+               break;
+           case 3:
+               System.out.println(3);
+               break;
+           default:
+               System.out.println("Número diferente de 1, 2 e 3.");
+       }
+   }
+}
---
+++ Atividade-01/Quest_05.java
@@ -0,0 +1,14 @@
+package LP2;
+
+public class Quest_05 {
+    
+    public static void main(String[]args){
+    
+        for(int i=1; i<=10 ; i++){
+         for(int j=i; j>=1 ; j--){
+           System.out.printf("%d ", j);
+       }
+       System.out.println();
+     }
+    }
+}
